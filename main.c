141defb347ff81ae8abbdaeecd6750f6f9863857
chandna singh <chandan.singh@amd.com>
2022-02-20 23:21:00 +0530

    Just figured out its better to learn in linux

--- main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("Hello hell!!! \n");
-	return 0;
-}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <add.h>
+
+int add(int a, int b);
+
+int main(){
+	printf("Hello hell!!! \n");
+	int res = add(2,6);
+	std::cout << res  << std::endl;
+	return 0;
+}
